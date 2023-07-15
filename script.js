7b54acc763d590cd91b7cad8a1429282c15d7009
nzerachi <62904003+nzerachi@users.noreply.github.com>
2023-07-15 21:34:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let ProjectTitle = document.getElementById('ProjectTitle');
+let Other = document.getElementsByClassName('Other');
+
+function hideOther(){
+    Other.style.display = 'none';
+}
+
+Window.onload.hideOther
+
+/* 
+
+function OtherOption() {
+    if (document.getElementById('ProjectTitle')==Other) {
+        document.getElementById('Other').style.display='block';
+    } else {
+        document.getElementById('Other').style.display='block';
+    }
+}
+
+
+
+function OtherOption() {
+    
+
+Other.attr('disabled');
+Other.parentElement.hide;
+
+
+if (ProjectTitle == 'Other') {
+    Other.removeattr('disabled');
+    Other.parentElement.hide;
+    }
+else {
+    Other.attr('disabled')
+    Other.parentElement.hide
+}
+}
+ */
+
+
+
+var POC
